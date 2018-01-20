2dd857115e42c5178e05a1d20d18e7a531a9fa93
Ryota Kawamura <dk.mat.18.of.a.challenge@gmail.com>
2018-01-20 14:55:37 +0900

    Save the record correctly and clearly

--- compare2MSE.py
+++ compare2MSE.py
@@ -21,12 +21,9 @@ def compare_images(imageA, imageB):
 
 wb = Workbook()
 ws = wb.add_sheet("10vs10", cell_overwrite_ok=True)
-ws.write(0, 0, "main")
-ws.write(1, 0, "sub")
-ws.write(2, 0, "esm")
 
-path1 = "./images1/"
-path2 = "./images2/"
+path1 = "./MSE/c_normal_1/"
+path2 = "./MSE/c_normal_2/"
 images1 = []
 images2 = []
 
@@ -38,17 +35,24 @@ for x in os.listdir(path2):
     if os.path.isfile(path2 + x):
         images2.append(path2 + x)
 
+print(images1)
+print(images2)
+
 for a in range(len(images1)):
-    ws.write(0, a + 1, "img" + str(a))
+    ws.write(4 * a, 0, "c_normal_1")
+    ws.write(4 * a + 1, 0, "c_normal_2")
+    ws.write(4 * a + 2, 0, "mse")
+    ws.write(4 * a, 1, "img" + str(a))
     for b in range(len(images2)):
         img1 = cv2.imread(images1[a])
         img2 = cv2.imread(images2[b])
 
         img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
         img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-        esm = compare_images(img1_gray, img2_gray)
+        mean_square_error = compare_images(img1_gray, img2_gray)
+        # print(mean_square_error)
 
-        ws.write(1, b + 1,  "img" + str(b))
-        ws.write(2, b + 1, str(esm))
+        ws.write(4 * a + 1, b + 1,  "img" + str(b))
+        ws.write(4 * a + 2, b + 1, str(mean_square_error))
 
-wb.save("./result.xls")
+wb.save("./result_c_normal.xls")
