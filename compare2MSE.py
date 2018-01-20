f845514585847db26f37685be75f8fc4e2aba779
Ryota Kawamura <dk.mat.18.of.a.challenge@gmail.com>
2018-01-20 17:56:13 +0900

    Add ssim to xls

--- compare2MSE.py
+++ compare2MSE.py
@@ -16,14 +16,14 @@ def mse(imageA, imageB):
 def compare_images(imageA, imageB):
     m = mse(imageA, imageB)
     s = ssim(imageA, imageB)
-    return m
+    return m, s
 
 
 wb = Workbook()
 ws = wb.add_sheet("10vs10", cell_overwrite_ok=True)
 
-path1 = "./MSE/c_normal_1/"
-path2 = "./MSE/c_normal_2/"
+path1 = "hoge1"
+path2 = "hoge2"
 images1 = []
 images2 = []
 
@@ -39,20 +39,23 @@ print(images1)
 print(images2)
 
 for a in range(len(images1)):
-    ws.write(4 * a, 0, "c_normal_1")
-    ws.write(4 * a + 1, 0, "c_normal_2")
-    ws.write(4 * a + 2, 0, "mse")
-    ws.write(4 * a, 1, "img" + str(a))
+    ws.write(5 * a, 0, "hoge1")
+    ws.write(5 * a + 1, 0, "hoge2")
+    ws.write(5 * a + 2, 0, "mse")
+    ws.write(5 * a + 3, 0, "ssim")
+    ws.write(5 * a, 1, "img" + str(a))
     for b in range(len(images2)):
         img1 = cv2.imread(images1[a])
         img2 = cv2.imread(images2[b])
 
         img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
         img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-        mean_square_error = compare_images(img1_gray, img2_gray)
+        mean_square_error, structural_similarity = compare_images(
+            img1_gray, img2_gray)
         # print(mean_square_error)
 
-        ws.write(4 * a + 1, b + 1,  "img" + str(b))
-        ws.write(4 * a + 2, b + 1, str(mean_square_error))
+        ws.write(5 * a + 1, b + 1,  "img" + str(b))
+        ws.write(5 * a + 2, b + 1, str(mean_square_error))
+        ws.write(5 * a + 2, b + 1, str(mean_square_error))
 
-wb.save("./result_c_normal.xls")
+wb.save("./result.xls")
